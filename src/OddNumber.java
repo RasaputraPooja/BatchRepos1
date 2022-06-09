28197e84f83ec22f2c9e8a31680ca92322f8f22e
Rasaputra pooja <pooja.rasaputra@capgemini.com>
2022-06-09 22:54:23 +0530

    Second commit

---
+++ src/OddNumber.java
@@ -0,0 +1,17 @@
+//print odd numbers from 1 to 100 in reverse order
+public class OddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=99;i>=1;i--)
+		{
+			if(i%2!=0)
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+
